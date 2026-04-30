b2ae5d2f0826869a5d7c3442eae2b2653ab10788
Eduardo Almeida <eduardofralmeida745@gmail.com>
2026-04-30 19:36:51 -0300

    Exercicios dia 5

---
+++ dia05/exercicio1.py
@@ -0,0 +1,6 @@
+# Números pares
+# Mostre todos os pares de 1 até 50
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
---
+++ dia05/exercicio3.py
@@ -0,0 +1,8 @@
+# Inverter texto
+# Peça uma palavra e mostre ela invertida
+
+palavra = input('Digite uma palavra ou frase: ')
+
+palavra_invertida = palavra[::-1]
+
+print(f'O inverso de {palavra} é {palavra_invertida} ')
